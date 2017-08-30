be48e60bff660e78d32328c8c9342929db3ca01e
Evgeniy Goryuchkin <goryuchkin@gmail.com>
2017-08-30 19:19:41 +0300

    reload nginx after successfull query

--- nginx-cloudflare.py
+++ nginx-cloudflare.py
@@ -5,32 +5,38 @@ import requests
 import configparser
 from optparse import OptionParser
 
-optionParser = OptionParser()
-optionParser.add_option("-c", "--config", dest="config_filename", help="Config file", metavar="FILE")
-(options, args) = optionParser.parse_args()
+try:
+	optionParser = OptionParser()
+	optionParser.add_option("-c", "--config", dest="config_filename", help="Config file", metavar="FILE")
+	(options, args) = optionParser.parse_args()
 
-configParser = configparser.ConfigParser()
-configParser.read(options.config_filename)
-config = configParser['cloudflare']
+	configParser = configparser.ConfigParser()
+	configParser.read(options.config_filename)
+	config = configParser['cloudflare']
 
-content = [
-	'# https://www.cloudflare.com/ips',
-	'# Updated: {:%d.%m.%Y %H:%M:%S}'.format(datetime.now()),
-	''
-]
+	content = [
+		'# https://www.cloudflare.com/ips',
+		'# Updated: {:%d.%m.%Y %H:%M:%S}'.format(datetime.now()),
+		''
+	]
 
-request = requests.get(config['ipv4'])
-for line in request.iter_lines():
-	content.append('set_real_ip_from %s;' % line.decode('utf-8'))
-content.append('')
+	request = requests.get(config['ipv4'])
+	for line in request.iter_lines():
+		content.append('set_real_ip_from %s;' % line.decode('utf-8'))
+	content.append('')
 
-request = requests.get(config['ipv6'])
-for line in request.iter_lines():
-	content.append('set_real_ip_from %s;' % line.decode('utf-8'))
-content.append('')
+	request = requests.get(config['ipv6'])
+	for line in request.iter_lines():
+		content.append('set_real_ip_from %s;' % line.decode('utf-8'))
+	content.append('')
 
-content.append('real_ip_header CF-Connecting-IP;')
+	content.append('real_ip_header CF-Connecting-IP;')
 
-with open(config['output'], 'w') as file:
-	file.writelines(map(lambda item: item + '\n', content))
-	file.close()
+	with open(config['output'], 'w') as file:
+		file.writelines(map(lambda item: item + '\n', content))
+		file.close()
+except Exception as e:
+	print(e)
+	exit(1)
+
+exit(0)
